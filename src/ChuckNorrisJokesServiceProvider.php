69af1a35c4521494237d93ce3bf41e974c2a1018
Irvan Dinda Prakoso <irvandindaprakoso@users.noreply.github.com>
2021-01-02 01:12:37 +0000

    Apply fixes from StyleCI

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -9,9 +9,9 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()){
+        if ($this->app->runningInConsole()) {
             $this->commands([
-                ChuckNorrisJoke::class
+                ChuckNorrisJoke::class,
             ]);
         }
     }
